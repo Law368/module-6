7a19bd3074db0ce6b1260bf10a0107ef319de91a
Nikita Zhulev <Nikita_Zhulev@epam.com>
2021-12-24 17:32:04 +0300

    implementation of cache saving

--- src/memoize.js
+++ src/memoize.js
@@ -1,7 +1,23 @@
-export function memoize(test) {
-    if (typeof test !== "function") {
+function inc(x) {
+    return x + 1;
+}
+
+export function memoize(func) {
+    if (typeof func !== "function") {
         return null;
     }
-
-    return test;
+    const cache = {};
+    return function(...args) {
+        const key = args[0];
+        if (key in cache) {
+            return cache[key];
+        }
+        {
+            const result = func(key);
+            cache[key] = result;
+            return result;
+        }
+    };
 }
+
+memoize(inc);
