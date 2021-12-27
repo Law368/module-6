2ae6902871ab4ace7a1857b0cf75b23b5de94651
Nikita Zhulev <Nikita_Zhulev@epam.com>
2021-12-27 15:16:56 +0300

    implement memoization function

--- src/memoize.js
+++ src/memoize.js
@@ -1,19 +1,28 @@
-function inc(x) {
-    return x + 1;
+function inc(...args) {
+    return args.reduce((previous, current) => previous + current) + 1;
 }
+function generateKey(args) {
+    let key = "";
+    for (let i = 0; i < args.length; i += 1) {
+        key += String(args[i]) + typeof args[i];
+    }
 
+    return key;
+}
 export function memoize(func) {
     if (typeof func !== "function") {
         return null;
     }
+
     const cache = {};
-    return function(...args) {
-        const key = args[0];
+    return function returnResult(...args) {
+        const key = generateKey(args);
         if (key in cache) {
             return cache[key];
         }
+
         {
-            const result = func(key);
+            const result = func.apply(this, args);
             cache[key] = result;
             return result;
         }
